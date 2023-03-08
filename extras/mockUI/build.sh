00fac5e0a1adb923ac47b99515d997a3820a7115
James Foster <github@jgfoster.net>
2023-03-08 14:50:32 -0800

    See https://github.com/wxWidgets/Phoenix/issues/2296.

--- extras/mockUI/build.sh
+++ extras/mockUI/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-if [ ${PWD##*/} != "GUI" ]; then
-  cd GUI
+if [ ${PWD##*/} != "mockUI" ]; then
+  cd extras/mockUI
 fi
-../extras/scripts/install_libraries.sh
+../scripts/install_libraries.sh
 python3 -m pip install wxPython pybind11
+echo "line 8"
 x=$(bundle exec which arduino_library_location.rb)
+echo "line 10: $x"
 y=$(echo "${x%/bin/*}/bundler/gems/arduino_ci-*")
+echo "line 12: $y"
 export ARDUINO_CI=${y}/cpp/arduino
-export TC_PATH=$((cd ..; pwd))/src
+export TC_PATH=$((cd ..; cd ..; pwd))/src
+echo "line 15: $TC_PATH"
+exit 0
 make clean
 (mkdir -p .build; cd .build; make -f ../Makefile)
 python3 ./TankController.py
