7aeca8880f077d8ffaad29deb7e63bdaa8cbed85
James Foster <git@jgfoster.net>
2023-04-07 09:59:44 -0700

    WIP: add debug code to build script

--- extras/mockUI/build.sh
+++ extras/mockUI/build.sh
@@ -4,16 +4,13 @@ if [ ${PWD##*/} != "mockUI" ]; then
   cd extras/mockUI
 fi
 ../scripts/install_libraries.sh
-python3 -m pip install wxPython pybind11
-echo "line 8"
+python3 -m pip install pybind11 wxPython
 x=$(bundle exec which arduino_library_location.rb)
-echo "line 10: $x"
 y=$(echo "${x%/bin/*}/bundler/gems/arduino_ci-*")
-echo "line 12: $y"
 export ARDUINO_CI=${y}/cpp/arduino
-export TC_PATH=$((cd ..; cd ..; pwd))/src
-echo "line 15: $TC_PATH"
-exit 0
+cd ../..
+export TC_PATH=$(pwd)/src
+cd extras/mockUI
 make clean
 (mkdir -p .build; cd .build; make -f ../Makefile)
 python3 ./TankController.py
